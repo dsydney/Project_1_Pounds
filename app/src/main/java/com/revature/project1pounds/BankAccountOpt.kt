737a0802c7cb224692ff532ccdb4b27e42c96bca
dsydney <100386938+dsydney@users.noreply.github.com>
2022-03-14 12:59:54 -0500

    Fixed some bug in manifest that was preventing it from running

--- app/src/main/java/com/revature/project1pounds/BankAccountOpt.kt
+++ app/src/main/java/com/revature/project1pounds/BankAccountOpt.kt
@@ -142,8 +142,10 @@ fun banknameField(entry:String)
         )
     }
 }
-@Preview(showBackground = true)
+/*@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-bankAccountInfo()
+    bankAccountInfo()
 }
+
+ */
