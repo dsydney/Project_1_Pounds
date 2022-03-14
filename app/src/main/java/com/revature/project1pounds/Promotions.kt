4cf7fe33570f795d537adb44fa7b7c4ec1489932
jonathancastaneda-revature <jonathan741@revature.net>
2022-03-14 09:01:47 -0700

    adding parts of promotions

--- app/src/main/java/com/revature/project1pounds/Promotions.kt
+++ app/src/main/java/com/revature/project1pounds/Promotions.kt
@@ -1,14 +1,21 @@
 package com.revature.project1pounds
 
+import android.graphics.drawable.AnimatedImageDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.revature.project1pounds.ui.theme.Project1PoundsTheme
 
@@ -16,9 +23,44 @@ class Promotions : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Project1PoundsTheme {
 
-            }
         }
     }
+}
+@Composable
+fun promotionsScreen()
+{
+    TaskBar()
+    Column(modifier=Modifier.verticalScroll(rememberScrollState()),horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Row {
+            addCard()
+            addCard()
+        }
+        Row {
+            addCard()
+            addCard()
+        }
+        Row {
+            addCard()
+            addCard()
+        }
+        Row {
+            addCard()
+            addCard()
+        }
+        Row {
+            addCard()
+            addCard()
+        }
+    }
+}
+@Composable
+fun addCard()
+{
+    Image(painter = painterResource(id=R.drawable.gpay))
+}
+data class addImages(image )
+{
+
 }
