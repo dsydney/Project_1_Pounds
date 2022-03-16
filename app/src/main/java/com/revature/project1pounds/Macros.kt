eff49fe7db096784f3267d34b634ee8768d0659f
tatebl <brandon.tate15@gmail.com>
2022-03-15 21:59:14 -0400

    Added formatting/coloring to Macros page

--- app/src/main/java/com/revature/project1pounds/Macros.kt
+++ app/src/main/java/com/revature/project1pounds/Macros.kt
@@ -3,10 +3,14 @@ package com.revature.project1pounds
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
@@ -30,8 +34,7 @@ fun MacroScreen() {
         TopBar()
         Column(modifier = Modifier
             .fillMaxSize()
-            .absolutePadding(left = 24.dp, right = 24.dp, top = 20.dp, bottom = 20.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp)
+            .absolutePadding(left = 24.dp, right = 24.dp, top = 20.dp, bottom = 20.dp)
             ) {
             CalorieLimit(x = 2000)
             Sliders()
@@ -57,7 +60,16 @@ fun TopBar() {
 
 @Composable
 fun CalorieLimit(x:Int) {
-    Text(text = "Daily Calorie Limit: $x")
+    Column(modifier = Modifier.padding(top = 8.dp)) {
+        Row() {
+            Text(text = "Daily Calorie Limit: ",
+                style=MaterialTheme.typography.h6)
+            Text(text = "$x", color = Color.Red,
+                style=MaterialTheme.typography.h6)
+        }
+    }
+
+
 }
 
 @Composable
@@ -65,51 +77,86 @@ fun Sliders() {
     var proteinSliderValue by remember { mutableStateOf(1f) }
     var carbsSliderValue by remember { mutableStateOf(1f) }
     var fatsSliderValue by remember { mutableStateOf(1f) }
-
+    Column(modifier = Modifier.padding(top = 16.dp)) {
+        
+    }
+    Text(text = "Protein: ${proteinSliderValue.roundToInt()}",
+        color = Color.Blue,
+        modifier = Modifier.padding(top = 16.dp)
+    )
     Slider(
         value = proteinSliderValue,
         valueRange = 0f..300f,
+        colors = SliderDefaults.colors(
+            activeTrackColor = Color.Blue,
+            inactiveTrackColor = Color.LightGray,
+            thumbColor = Color.Blue),
         modifier = Modifier.padding(4.dp),
         onValueChange = { newValue ->
             proteinSliderValue = newValue
         }
     )
-    Text(
-        text = "Protein: ${proteinSliderValue.roundToInt()}",
-        modifier = Modifier.padding(4.dp)
-    )
 
+    Text(
+        text = "Carbs: ${carbsSliderValue.roundToInt()}",
+        color = Color.Red,
+        modifier = Modifier.padding(top = 8.dp)
+    )
     Slider(
         value = carbsSliderValue,
         valueRange = 0f..300f,
+        colors = SliderDefaults.colors(
+            activeTrackColor = Color.Red,
+            inactiveTrackColor = Color.LightGray,
+            thumbColor = Color.Red),
         modifier = Modifier.padding(4.dp),
         onValueChange = { newValue ->
             carbsSliderValue = newValue
         }
     )
+
     Text(
-        text = "Carbs: ${carbsSliderValue.roundToInt()}",
-        modifier = Modifier.padding(4.dp)
+        text = "Fats: ${fatsSliderValue.roundToInt()}",
+        color = Color(0xffc9c422.toInt()),
+        modifier = Modifier.padding(top = 8.dp)
     )
     Slider(
         value = fatsSliderValue,
         valueRange = 0f..200f,
-        modifier = Modifier.padding(8.dp),
+        colors = SliderDefaults.colors(
+            activeTrackColor = Color(0xffc9c422.toInt()),
+            inactiveTrackColor = Color.LightGray,
+            thumbColor = Color(0xffc9c422.toInt())),
+        modifier = Modifier.padding(4.dp),
         onValueChange = { newValue ->
             fatsSliderValue = newValue
         }
     )
-    Text(
-        text = "Fats: ${fatsSliderValue.roundToInt()}",
-        modifier = Modifier.padding(4.dp)
-    )
+
 }
 
 @Composable
 fun HealthyHints() {
-    Text(text = "Healthy hints:")
-    Text(text = "Eat your vegetables")
-    Text(text = "After that eat more vegetables")
+    var hints = listOf(
+        "Healthy eating is a way of life, so it's important to establish routines that are simple and realistic",
+        "Whole grains are a very important part of a healthy, balanced diet",
+        "Nothing spells health like H-2-O"
+
+    )
+    Column(modifier = Modifier
+        .padding(top = 20.dp)
+        .border(1.dp, Color.LightGray, RectangleShape),
+        verticalArrangement = Arrangement.spacedBy(10.dp)) {
+        Text(text = "Healthy hints:",
+            style=MaterialTheme.typography.h5,
+            modifier = Modifier.padding(4.dp))
+        for(i in hints)
+            Text(text = i,
+            fontFamily =FontFamily.SansSerif,
+            modifier = Modifier.padding(4.dp))
+
+    }
+
 }
 
 
