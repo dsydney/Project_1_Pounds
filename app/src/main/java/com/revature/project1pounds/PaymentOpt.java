87f506c51f5f49ed006cd33ac7f9c6eda1822920
jonathancastaneda-revature <jonathan741@revature.net>
2022-03-17 20:53:00 -0700

    Added Registration page, and changed color on Splash Image

--- app/src/main/java/com/revature/project1pounds/PaymentOpt.java
+++
@@ -1,6 +0,0 @@
-package com.revature.project1pounds;
-
-import android.app.Activity;
-
-public class PaymentOpt extends Activity {
-}
--- app/src/main/java/com/revature/project1pounds/ui/theme/PaymentOptions.java
+++
@@ -1,6 +0,0 @@
-package com.revature.project1pounds.ui.theme;
-
-import android.app.Activity;
-
-public class PaymentOptions extends Activity {
-}
