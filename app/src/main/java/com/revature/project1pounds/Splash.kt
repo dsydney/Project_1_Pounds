0ed5a8e7c8941085d602a5c9b3acf82752c4cb72
DietLasagna <100707359+DietLasagna@users.noreply.github.com>
2022-03-18 16:17:34 -0500

    Added image back to Splash.kt (fixed)

--- app/src/main/java/com/revature/project1pounds/Splash.kt
+++ app/src/main/java/com/revature/project1pounds/Splash.kt
@@ -40,14 +40,15 @@ fun StartupSplash() {
             .fillMaxSize()
             .clickable(onClick = { context.startActivity(Intent(context, Login::class.java)) })
             .background(MaterialTheme.colors.primary)
-        )
-        Image(
-            painter = painterResource(R.drawable.pounds),
-            contentDescription = "",
-            modifier = Modifier.fillMaxSize(),
-            alignment = Alignment.Center,
-            contentScale = ContentScale.Fit,
-        )
+        ) {
+            Image(
+                painter = painterResource(R.drawable.pounds),
+                contentDescription = "",
+                modifier = Modifier.fillMaxSize(),
+                alignment = Alignment.Center,
+                contentScale = ContentScale.Fit,
+            )
+        }
     }
 }
 
