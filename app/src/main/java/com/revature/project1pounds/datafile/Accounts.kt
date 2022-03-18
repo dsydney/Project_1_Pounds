658b3e8ac1af5e50f4e50a9b2632109556f42a8b
jonathancastaneda-revature <jonathan741@revature.net>
2022-03-18 09:43:53 -0700

    Added account classes and map

--- app/src/main/java/com/revature/project1pounds/datafile/Accounts.kt
+++ app/src/main/java/com/revature/project1pounds/datafile/Accounts.kt
@@ -1,2 +1,32 @@
 package com.revature.project1pounds.datafile
 
+val accountList:MutableMap<String,account> = mutableMapOf(
+    "david@gmail.com" to account("David",
+        "Sydney",
+        "david@gmial.com",
+        "sydney",
+        2000,
+        currentCalories = null),
+
+    "jonathan@gmail.com" to account("Jonathan",
+        "Castaneda",
+        "castaneda",
+        "jonathan@gmail.com",
+        2000,
+        currentCalories = null),
+    "michael@gmail.com" to account("Michael",
+        "Adams",
+        "michael@gmail.com",
+        "adams",
+        2000,
+        currentCalories = null),
+    "brandon@gmail.com" to account("Brandon",
+        "Tate",
+        "brandon@gmail.com",
+        "tate",
+        2000,
+        currentCalories = null)
+)
+
+
+
