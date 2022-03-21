412f3e2cd47615b84532e502b48b569d836e4b3b
dsydney <100386938+dsydney@users.noreply.github.com>
2022-03-20 23:14:20 -0500

    updated progress and penalties

--- app/src/main/java/com/revature/project1pounds/Penalties.kt
+++ app/src/main/java/com/revature/project1pounds/Penalties.kt
@@ -14,12 +14,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.revature.project1pounds.ui.theme.Project1PoundsTheme
@@ -85,80 +91,362 @@ fun PenaltiesMain() {
 
                 Column(modifier = Modifier.padding(20.dp)) {
 
-                    Text(text = "Monthly Progress: -$34")
+                    Row() {
 
-                    //Insert image of a graph here
-                    val painter = painterResource(id = R.drawable.__bfuktr4h4mzmy5uhuez_za)
-                    val description = "Bar Chart"
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
+                        Text(text = "March\n\n",
+                            fontWeight = FontWeight.Bold)
 
-                        Image(
-                            painter = painter,
-                            contentDescription = description,
-                            contentScale = ContentScale.FillWidth
+                        Text(text = "\n     Penalties: $5\n",
+                            fontWeight = FontWeight.Bold,
+                            fontStyle = FontStyle.Italic
                         )
 
+                    }
+                    //Insert image of a graph here
+                    Card(
+
+                        shape = RoundedCornerShape(10.dp),
+                        backgroundColor = DarkGray
+
+                    ) {
+
+
+                        Row(
+
+                            Modifier.padding(10.dp),
+
+                            ) {
+
+                            Column() {
+
+                                Text(
+                                    "Days over budget: 5",
+                                    color = Color.Red,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    "Days on budget: 12",
+                                    color = Color.Green,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    "Days under budget: 4",
+                                    color = Color.Yellow,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    "Days remaining: 10",
+                                    color = Color.LightGray,
+                                    fontWeight = FontWeight.Bold
+                                )
+
+                            }
+
+                            Spacer(Modifier.width(20.dp))
+
+                            Canvas(modifier = Modifier.fillMaxSize().padding(10.dp)) {
+
+                                val canvasWidth = size.width
+                                val canvasHeight = size.height
+
+                                drawArc(
+
+                                    color = Color.Red,
+                                    startAngle = 0f,
+                                    sweepAngle = 58f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                                drawArc(
+
+                                    color = Green,
+                                    startAngle = 58f,
+                                    sweepAngle = 139f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                                drawArc(
+
+                                    color = Color.Yellow,
+                                    startAngle = 197f,
+                                    sweepAngle = 46f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                                drawArc(
+
+                                    color = Color.LightGray,
+                                    startAngle = 243f,
+                                    sweepAngle = 117f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                            }
+                        }
                     }
 
                 }
 
             }
 
-            Spacer(Modifier.height(10.dp))
-
+            //Feb
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(10.dp),
                 elevation = 12.dp,
                 shape = RoundedCornerShape(10.dp),
-                border = BorderStroke(3.dp, Black)
+                border = BorderStroke(3.dp, Color.Black)
 
             ) {
 
                 Column(modifier = Modifier.padding(20.dp)) {
 
-                    Text(text = "Annual Progress: -$112")
+                    Row() {
 
-                    //Insert image of a graph here
-                    val painter = painterResource(id = R.drawable.ierbv)
-                    val description = "Bar Chart"
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(color = White)
-                    ) {
+                        Text(text = "February\n\n",
+                            fontWeight = FontWeight.Bold)
 
-                        Image(
-                            painter = painter,
-                            contentDescription = description,
-                            contentScale = ContentScale.FillWidth
+                        Text(text = "\n     Penalties: $8\n",
+                            fontWeight = FontWeight.Bold,
+                            fontStyle = FontStyle.Italic
                         )
 
                     }
+                    //Insert image of a graph here
+                    Card(
 
+                        shape = RoundedCornerShape(10.dp),
+                        backgroundColor = DarkGray
+
+                    ) {
+
+
+                        Row(
+
+                            Modifier.padding(10.dp),
+
+                            ) {
+
+                            Column() {
+
+                                Text(
+                                    "Days over budget: 8",
+                                    color = Color.Red,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    "Days on budget: 14",
+                                    color = Color.Green,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    "Days under budget: 6",
+                                    color = Color.Yellow,
+                                    fontWeight = FontWeight.Bold
+                                )
+
+                            }
+
+                            Spacer(Modifier.width(20.dp))
+
+                            Canvas(modifier = Modifier.fillMaxSize().padding(10.dp)) {
+
+                                val canvasWidth = size.width
+                                val canvasHeight = size.height
+
+                                drawArc(
+
+                                    color = Color.Red,
+                                    startAngle = 0f,
+                                    sweepAngle = 103f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                                drawArc(
+
+                                    color = Green,
+                                    startAngle = 103f,
+                                    sweepAngle = 180f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                                drawArc(
+
+                                    color = Color.Yellow,
+                                    startAngle = 283f,
+                                    sweepAngle = 77f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                            }
+                        }
+                    }
 
                 }
 
             }
 
-        }
+            //Jan
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                elevation = 12.dp,
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(3.dp, Color.Black)
 
-    }
+            ) {
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.End
-    ) {
+                Column(modifier = Modifier.padding(20.dp)) {
 
-        Button(onClick = {
-            context.startActivity(Intent(context, MainActivity::class.java))
-        }) {
-            Text(text = "Home")
+                    Row() {
+
+                        Text(text = "January\n\n",
+                            fontWeight = FontWeight.Bold)
+
+                        Text(text = "\n     Penalties: $12\n",
+                            fontWeight = FontWeight.Bold,
+                            fontStyle = FontStyle.Italic
+                        )
+
+                    }
+                    //Insert image of a graph here
+                    Card(
+
+                        shape = RoundedCornerShape(10.dp),
+                        backgroundColor = DarkGray
+
+                    ) {
+
+
+                        Row(
+
+                            Modifier.padding(10.dp),
+
+                            ) {
+
+                            Column() {
+
+                                Text(
+                                    "Days over budget: 12",
+                                    color = Color.Red,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    "Days on budget: 18",
+                                    color = Color.Green,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    "Days under budget: 1",
+                                    color = Color.Yellow,
+                                    fontWeight = FontWeight.Bold
+                                )
+
+                            }
+
+                            Spacer(Modifier.width(20.dp))
+
+                            Canvas(modifier = Modifier.fillMaxSize().padding(10.dp)) {
+
+                                val canvasWidth = size.width
+                                val canvasHeight = size.height
+
+                                drawArc(
+
+                                    color = Color.Red,
+                                    startAngle = 0f,
+                                    sweepAngle = 139f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                                drawArc(
+
+                                    color = Green,
+                                    startAngle = 139f,
+                                    sweepAngle = 209f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                                drawArc(
+
+                                    color = Color.Yellow,
+                                    startAngle = 348f,
+                                    sweepAngle = 12f,
+                                    useCenter = false,
+                                    size = Size(
+                                        width = canvasWidth * 0.7f,
+                                        height = canvasWidth * 0.7f
+                                    ),
+                                    style = Stroke(width = 40f)
+
+                                )
+
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+            Spacer(Modifier.height(100.dp))
+
         }
 
     }
--- app/src/main/java/com/revature/project1pounds/Progress.kt
+++ app/src/main/java/com/revature/project1pounds/Progress.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -22,6 +23,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.revature.project1pounds.ui.theme.Project1PoundsTheme
+import com.revature.project1pounds.ui.theme.Red400
+import com.revature.project1pounds.ui.theme.Red700
 
 class Progress : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,16 +56,6 @@ fun ProgressMain() {
 
         TopAppBar(title = { Text(text = "Daily Progress") })
 
-        Canvas(modifier = Modifier.fillMaxWidth()) {
-            val canvasWidth = size.width
-            val canvasHeight = size.height
-            drawCircle(
-                color = Color.Blue,
-                center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
-                radius = size.minDimension / 4
-            )
-        }
-
         Column(
 
             modifier = Modifier
@@ -74,6 +67,7 @@ fun ProgressMain() {
 
             Spacer(Modifier.height(5.dp))
 
+            //Snapshot Card
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -86,21 +80,28 @@ fun ProgressMain() {
 
                 Column(modifier = Modifier.padding(20.dp)) {
 
-                    Text(text = "Calories: 2210")
+                    Text(text = "Today: \n\n",
+                        fontWeight = FontWeight.Bold)
 
-                    Text(text = "Macros\nProtein: 140g   25%\nCarbohydrates: 210g   38%\nFat: 91g   37%")
+                    Text(text = "Calories: 2210",
+                        fontWeight = FontWeight.Bold)
 
-                    Text(text = "Steps: 5283")
+                    Text(text = "Macros\nProtein: 140g   25%\nCarbohydrates: 210g   38%\nFat: 91g   37%",
+                        fontWeight = FontWeight.Bold)
+
+                    Text(text = "Steps: 5283",
+                        fontWeight = FontWeight.Bold)
 
                 }
             }
 
-            Spacer(Modifier.height(10.dp))
+            //Spacer(Modifier.height(10.dp))
 
+            //Macros Card
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(300.dp)
+                    .height(210.dp)
                     .padding(10.dp),
                 elevation = 12.dp,
                 shape = RoundedCornerShape(10.dp),
@@ -111,24 +112,25 @@ fun ProgressMain() {
 
                 Column(modifier = Modifier.padding(20.dp)) {
 
-                    Text(text = "Today")
+                    Text(text = "Macros\n\n",
+                        fontWeight = FontWeight.Bold)
 
                     Row() {
 
                         Column() {
 
                             Text(
-                                "Protein",
+                                "Protein 25%",
                                 color = Color.DarkGray,
                                 fontWeight = FontWeight.Bold
                             )
                             Text(
-                                text = "Carbohydrates",
+                                text = "Carbohydrates 38%",
                                 color = Color.LightGray,
                                 fontWeight = FontWeight.Bold
                             )
                             Text(
-                                text = "Fat",
+                                text = "Fat 37%",
                                 color = Color.Gray,
                                 fontWeight = FontWeight.Bold
                             )
@@ -176,8 +178,162 @@ fun ProgressMain() {
                 }
             }
 
+            //Steps Card
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .padding(10.dp),
+                elevation = 12.dp,
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(3.dp, Color.Black),
+
+
+                ) {
+
+                Column(modifier = Modifier.padding(20.dp)) {
+
+                    Text(text = "Step Count\n\n",
+                        fontWeight = FontWeight.Bold)
+
+                    Row() {
+
+                        Column() {
+
+                            Text(
+                                "Steps: 5283\nGoal: 7000",
+                                fontWeight = FontWeight.Bold
+                            )
+
+                        }
+
+                        Spacer(Modifier.width(20.dp))
+
+                        Canvas(modifier = Modifier.fillMaxSize()) {
+
+                            val canvasWidth = size.width
+                            val canvasHeight = size.height
+
+                            drawArc(
+
+                                color = Color.Blue,
+                                startAngle = 270f,
+                                sweepAngle =272f,
+                                useCenter = false,
+                                size = Size(width = canvasWidth * 0.7f, height = canvasWidth * 0.7f),
+                                style = Stroke(width = 40f)
+
+                            )
+
+                            drawArc(
+
+                                color = Color.DarkGray,
+                                startAngle = 182f,
+                                sweepAngle =88f,
+                                useCenter = false,
+                                size = Size(width = canvasWidth * 0.7f, height = canvasWidth * 0.7f),
+                                style = Stroke(width = 40f)
+
+                            )
+
+
+
+                        }
+                    }
+                }
+            }
+
+            //Calories Card
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(10.dp),
+                elevation = 12.dp,
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(3.dp, Color.Black),
+
+
+                ) {
+
+                Column(modifier = Modifier.padding(20.dp)) {
+
+                    Text(text = "Calories\n\n",
+                        fontWeight = FontWeight.Bold)
+
+                    Row() {
+
+                        Column() {
+
+                            Text(
+                                "Breakfast: 725 Calories",
+                                color = Color.LightGray,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                "Lunch: 816 Calories",
+                                color = Color.Gray,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                "Dinner: 669 Calories",
+                                color = Color.DarkGray,
+                                fontWeight = FontWeight.Bold
+                            )
+
+                        }
+
+                        Spacer(Modifier.width(20.dp))
+
+                        Canvas(modifier = Modifier.fillMaxSize()) {
+
+                            val canvasWidth = size.width
+                            val canvasHeight = size.height
+
+                            drawArc(
+
+                                color = Color.LightGray,
+                                startAngle = 0f,
+                                sweepAngle =118f,
+                                useCenter = true,
+                                size = Size(width = canvasWidth * 0.7f, height = canvasWidth * 0.7f),
+
+                            )
+
+                            drawArc(
+
+                                color = Color.Gray,
+                                startAngle = 118f,
+                                sweepAngle =133f,
+                                useCenter = true,
+                                size = Size(width = canvasWidth * 0.7f, height = canvasWidth * 0.7f),
+
+                            )
+
+                            drawArc(
+
+                                color = Color.DarkGray,
+                                startAngle = 251f,
+                                sweepAngle =109f,
+                                useCenter = true,
+                                size = Size(width = canvasWidth * 0.7f, height = canvasWidth * 0.7f),
+
+                            )
+
+                        }
+                    }
+                }
+            }
+
             Spacer(Modifier.height(100.dp))
 
         }
     }
 }
+
+
+
+
+
+
+
